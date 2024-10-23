9811a44948a18c09d09502b221cb7dc874f41293
SDF12344444 <115549409+SDF12344444@users.noreply.github.com>
2024-10-23 13:35:14 +0200

    Add files via upload

---
+++ htdocs_lab4_-dashborad_-xamp/lab4/2.php
@@ -0,0 +1,17 @@
+ <?php
+        echo "<h2>c)</h2>";
+
+        echo "Pętla while(): <br>";
+        $i = 0;
+        while ($i <= 5) {
+            echo "$i<br>";
+            $i++;
+        }
+
+        echo "<br>Pętla for(): <br>";
+        for ($j = 0; $j <= 5; $j++) {
+            echo "$j<br>";
+        }
+    ?>
+    
+    
